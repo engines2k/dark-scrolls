1d063e9380b76cd191ab2f878ad03be6fddd6f83
Kate Eckhart <eckhartalex@gmail.com>
2022-09-16 10:30:58 -0600

    Add moveable player.

--- Makefile
+++ Makefile
@@ -1,9 +1,53 @@
-SDLCFLAGS=-IC:/msys64/mingw64/include/SDL2 -Dmain=SDL_main
-SDLLIBS=-LC:/msys64/mingw64/lib -lmingw32 -mwindows -lSDL2main -lSDL2
+# Thanks to Job Vranish (https://spin.atomicobject.com/2016/08/26/makefile-c-projects/)
+TARGET_EXEC := dark_scrolls 
 
-dark_scrolls: main.cpp
-	g++ -g -Wall $(SDLCFLAGS) -o $@ $? $(SDLLIBS)
+BUILD_DIR := ./build
+DATA_DIR := ./data
+SRC_DIRS := ./src
 
-# Cleans up executables.
+# Find all the C and C++ files we want to compile
+# Note the single quotes around the * expressions. Make will incorrectly expand these otherwise.
+SRCS := $(shell find $(SRC_DIRS) -name '*.cpp' -or -name '*.c' -or -name '*.s')
+
+# String substitution for every C/C++ file.
+# As an example, hello.cpp turns into ./build/hello.cpp.o
+OBJS := $(SRCS:%=$(BUILD_DIR)/%.o)
+
+# String substitution (suffix version without %).
+# As an example, ./build/hello.cpp.o turns into ./build/hello.cpp.d
+DEPS := $(OBJS:.o=.d)
+
+# Every folder in ./src will need to be passed to GCC so that it can find header files
+INC_DIRS := $(shell find $(SRC_DIRS) -type d)
+# Add a prefix to INC_DIRS. So moduleA would become -ImoduleA. GCC understands this -I flag
+INC_FLAGS := $(addprefix -I,$(INC_DIRS))
+
+# The -MMD and -MP flags together generate Makefiles for us!
+# These files will have .d instead of .o as the output.
+CPPFLAGS := $(INC_FLAGS) -MMD -MP
+CFLAGS := -Wall --std=c++17 -g -Dmain=SDL_main
+CXXFLAGS := $(CFLAGS)
+LDFLAGS := -lmingw32 -mwindows -lSDL2main -lSDL2
+
+# The final build step.
+$(BUILD_DIR)/$(TARGET_EXEC): $(OBJS)
+	$(CXX) $(OBJS) -o $@ $(LDFLAGS)
+
+# Build step for C source
+$(BUILD_DIR)/%.c.o: %.cpp
+	mkdir -p $(dir $@)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
+
+# Build step for C++ source
+$(BUILD_DIR)/%.cpp.o: %.cpp
+	mkdir -p $(dir $@)
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $< -o $@
+
+.PHONY: clean
 clean:
-	rm dark_scrolls.exe
+	rm -r $(BUILD_DIR)
+
+# Include the .d makefiles. The - at the front suppresses the errors of missing
+# Makefiles. Initially, all the .d files will be missing, and we don't want those
+# errors to show up.
+-include $(DEPS)
---
+++ src/main.cpp
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <unordered_set>
+#include <mutex>
+#include <cstdint>
+#include <SDL2/SDL.h>
+#include <memory>
+#include <vector>
+//could be <SDL.h>
+
+const int WIDTH = 800, HEIGHT = 600;
+
+constexpr double FRAME_RATE = 1.0 / 60.0;
+constexpr int SUBPIXELS_IN_PIXEL = 1 << 15;
+
+class KeyboardManager {
+  public:
+  bool is_held(SDL_Scancode scan) {
+    return held.count(scan) >= 1;
+  }
+
+  void handle_keyevent(const SDL_KeyboardEvent &env) {
+    if(env.type == SDL_KEYDOWN && env.repeat == 0) {
+      held.insert(env.keysym.scancode);
+    }
+    if(env.type == SDL_KEYUP) {
+      held.erase(env.keysym.scancode);
+    }
+  }
+  private:
+  std::unordered_set<SDL_Scancode> held;
+};
+
+struct FrameCounter {
+  uint64_t rendered_frames = 0;
+  uint64_t scheduled_frames = 0;
+};
+
+class Game;
+
+class Sprite {
+  public:
+  Sprite(Game &game, int x, int y): game(game) {
+    this->pos_x = x;
+    this->pos_y = y;
+  }
+
+  virtual void draw() = 0;
+  virtual void tick() {}
+  int get_pos_x() const {
+    return pos_x;
+  }
+  int get_pos_y() const {
+    return pos_y;
+  }
+
+  virtual ~Sprite() {}
+
+  protected:
+  int pos_x;
+  int pos_y;
+  Game &game;
+};
+
+class Game {
+  public:
+  SDL_Renderer *renderer;
+  KeyboardManager keyboard;
+  int32_t tick_event_id;
+  FrameCounter frame_counter;
+  std::mutex frame_counter_lock;
+  std::vector<std::unique_ptr<Sprite>> sprite_list;
+
+  void tick() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    for (auto &sprite: sprite_list) {
+      sprite->tick();
+      sprite->draw();
+    }
+
+    SDL_RenderPresent(renderer);
+    auto frame_counter_lock = std::lock_guard(this->frame_counter_lock);
+    frame_counter.rendered_frames++;
+  }
+};
+
+class Player: public Sprite {
+  public:
+  Player(Game &game, int pos_x, int pos_y): Sprite(game, pos_x, pos_y) {}
+
+  virtual void tick() override {
+    int x_speed = 0;
+    int y_speed = 0;
+    if (game.keyboard.is_held(SDL_SCANCODE_W)) {
+      y_speed = -SPEED;
+    } else if (game.keyboard.is_held(SDL_SCANCODE_S)) {
+      y_speed = SPEED;
+    }
+
+    if (game.keyboard.is_held(SDL_SCANCODE_A)) {
+      x_speed = -SPEED;
+    } else if (game.keyboard.is_held(SDL_SCANCODE_D)) {
+      x_speed = SPEED;
+    }
+
+    pos_x += x_speed;
+    pos_y += y_speed;
+  }
+
+  virtual void draw() override {
+    SDL_Rect my_rect = SHAPE;
+    my_rect.x = pos_x / SUBPIXELS_IN_PIXEL;
+    my_rect.y = pos_y / SUBPIXELS_IN_PIXEL;
+
+    SDL_SetRenderDrawColor(game.renderer, RED, GREEN, BLUE, 255);
+    SDL_RenderFillRect(game.renderer, &my_rect);
+  }
+  private:
+  static constexpr uint32_t SPEED = (300 * FRAME_RATE) * SUBPIXELS_IN_PIXEL;
+  static constexpr SDL_Rect SHAPE = {.x = 0, .y = 0, .w = 30, .h = 30};
+  static constexpr uint8_t RED = 126;
+  static constexpr uint8_t GREEN = 219;
+  static constexpr uint8_t BLUE = 222;
+};
+
+uint32_t game_timer(uint32_t rate, void *game_ptr) {
+  Game &game = *static_cast<Game*>(game_ptr);
+
+  auto frame_counter_lock = std::lock_guard(game.frame_counter_lock);
+
+  if (game.frame_counter.scheduled_frames - game.frame_counter.rendered_frames >= 2) {
+    return rate;
+  }
+
+  game.frame_counter.scheduled_frames++;
+
+  SDL_Event event;
+  SDL_UserEvent userevent;
+
+  userevent.type = SDL_USEREVENT;
+  userevent.code = game.tick_event_id;
+  userevent.data1 = nullptr;
+  userevent.data2 = nullptr;
+
+  event.type = SDL_USEREVENT;
+  event.user = userevent;
+
+  SDL_PushEvent(&event);
+  return rate;
+}
+
+int main(int argc, char *argv[]) {
+  Game game;
+  SDL_Window *window;
+  if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+    printf("SDL_Init failed: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  game.tick_event_id = SDL_RegisterEvents(1);
+
+  window = SDL_CreateWindow("Hello, World!",
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED,
+                                        WIDTH, HEIGHT,
+                                        SDL_WINDOW_ALLOW_HIGHDPI);
+  if(window == NULL) {
+    printf("Could not create window: %s\n", SDL_GetError());
+    return 1;
+  }
+  
+  game.renderer = SDL_CreateRenderer(window, -1, 0);
+  game.sprite_list.push_back(std::make_unique<Player>(Player(game, 0, 0)));
+
+  SDL_TimerID tick_timer = SDL_AddTimer(FRAME_RATE * 1000, game_timer, &game);
+  
+  SDL_Event event;
+  while(1) {
+    if(SDL_WaitEvent(&event)) {
+      switch(event.type) {
+        case SDL_QUIT:
+          goto endgame;
+        case SDL_KEYUP:
+        case SDL_KEYDOWN:
+          game.keyboard.handle_keyevent(event.key);
+          break;
+        case SDL_USEREVENT:
+          if (event.user.code == game.tick_event_id) {
+            game.tick();
+          }
+          break;
+      }
+    } else {
+      printf("Event error: %s\n", SDL_GetError());
+      break;
+    }
+  }
+endgame:
+  SDL_RemoveTimer(tick_timer);
+
+  SDL_DestroyWindow(window);
+
+  SDL_Quit();
+  return 0;
+}
