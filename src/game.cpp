a6168301868b61fe2d07095b6df8740e0b5e3c42
Alex Eckhart <eckhartalex@gmail.com>
2022-11-30 18:28:29 -0700

    Move Game::tick.

--- src/game.cpp
+++ src/game.cpp
@@ -6,6 +6,52 @@ Game::Game(SDL_Renderer *renderer)
   collide_layers.resize(1);
 }
 
+void Game::tick() {
+  SDL_SetRenderTarget(renderer, nullptr);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  for (auto &collide_layer : collide_layers) {
+    collide_layer.clear();
+  }
+  current_level.add_colliders(collide_layers);
+
+  for (auto &sprite : sprite_list) {
+    sprite->add_colliders();
+  }
+
+  for (auto &sprite : sprite_list) {
+    sprite->tick();
+  }
+
+  current_level.handle_reactions();
+
+  current_level.draw();
+
+  for (auto &sprite : sprite_list) {
+    sprite->draw();
+  }
+
+  // set_cam_trans();
+  camera->calc_offset();
+  camera->calc_zoom();
+
+  std::vector<std::shared_ptr<Sprite>> next_sprite_list;
+
+  for (auto &sprite : sprite_list) {
+    if (sprite->is_spawned()) {
+      next_sprite_list.push_back(std::move(sprite));
+    }
+  }
+
+  sprite_list = std::move(next_sprite_list);
+  keyboard.reset_pressed();
+
+  SDL_RenderPresent(renderer);
+  auto frame_counter_lock = std::lock_guard(this->frame_counter_lock);
+  frame_counter.rendered_frames++;
+}
+
 #ifdef DARK_SCROLLS_WINDOWS
 std::filesystem::path Game::calc_data_path() {
   return std::filesystem::path(_pgmptr).parent_path() / "data";
--- src/main.cpp
+++ src/main.cpp
@@ -53,52 +53,6 @@ uint32_t game_timer(uint32_t rate, void *game_ptr) {
   return rate;
 }
 
-void Game::tick() {
-  SDL_SetRenderTarget(renderer, nullptr);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
-
-  for (auto &collide_layer : collide_layers) {
-    collide_layer.clear();
-  }
-  current_level.add_colliders(collide_layers);
-
-  for (auto &sprite : sprite_list) {
-    sprite->add_colliders();
-  }
-
-  for (auto &sprite : sprite_list) {
-    sprite->tick();
-  }
-
-  current_level.handle_reactions();
-
-  current_level.draw();
-
-  for (auto &sprite : sprite_list) {
-    sprite->draw();
-  }
-
-  // set_cam_trans();
-  camera->calc_offset();
-  camera->calc_zoom();
-
-  std::vector<std::shared_ptr<Sprite>> next_sprite_list;
-
-  for (auto &sprite : sprite_list) {
-    if (sprite->is_spawned()) {
-      next_sprite_list.push_back(std::move(sprite));
-    }
-  }
-
-  sprite_list = std::move(next_sprite_list);
-  keyboard.reset_pressed();
-
-  SDL_RenderPresent(renderer);
-  auto frame_counter_lock = std::lock_guard(this->frame_counter_lock);
-  frame_counter.rendered_frames++;
-}
-
 int main(int argc, char *argv[]) {
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
     printf("SDL_Init failed: %s\n", SDL_GetError());
